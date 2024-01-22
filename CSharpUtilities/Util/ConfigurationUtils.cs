41a9adcb58eb156fa6d39cbc4707c3b628e5c9af
Charles Paldino <charlespaldino@protonmail.com>
2024-01-22 02:54:26 -0500

    Rebuild
    Rebuilt to be a dll instead of random code files.

---
+++ CSharpUtilities/Util/ConfigurationUtils.cs
@@ -0,0 +1,38 @@
+﻿using System.Configuration;
+
+
+namespace DotNet_CSharp.Util
+{
+    public static class ConfigurationUtils
+    {
+        public static void updateConfigurationKey(string key, string value)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.AppSettings.Settings[key].Value = value;
+            config.Save(ConfigurationSaveMode.Modified);
+        }
+
+        /// <summary>
+        /// Takes 2 configuration keys and maps them into a dictionary.
+        /// </summary>
+        /// <param name="key1">Configuration key 1.</param>
+        /// <param name="key2">Configuration key 2.</param>
+        /// <param name="splitter">Character to split the keys with. IE: CSV ',' or PSV '|'</param>
+        /// <returns>A set of keys from input 1 mapped to keys from input 2.</returns>
+        public static Dictionary<string, string> mapConfigurationKeys(string key1, string key2, char splitter)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            Dictionary<string, string> mapping = new Dictionary<string, string>();
+
+            string[] keys_1 = config.AppSettings.Settings[key1].Value.Split(splitter);
+            string[] keys_2 = config.AppSettings.Settings[key2].Value.Split(splitter);
+
+            for (int x = 0; x > keys_1.Length; x++)
+            {
+                mapping.Add(keys_1[x], keys_2[x]);
+            }
+
+            return mapping;
+        }
+    }
+}
---
+++ CSharpUtilities/Util/IdentityUtils.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+
+namespace DotNet_CSharp.Util
+{
+    public static class IdentityUtils
+    {
+        public const String ROLE_ADMIN = "Admin";
+        public const String ROLE_OWNER = "Owner";
+
+        #region "Authentication"
+
+        public static ClaimsIdentity getOwnerClaim()
+        {
+            return new ClaimsIdentity
+                    (
+                        new List<Claim> { new Claim(ClaimTypes.Role, ROLE_OWNER) },
+                        CookieAuthenticationDefaults.AuthenticationScheme
+                    );
+        }
+
+        public static ClaimsPrincipal getAdminClaim()
+        {
+            return new ClaimsPrincipal(new ClaimsIdentity
+                    (
+                        new List<Claim> { new Claim(ClaimTypes.Role, ROLE_ADMIN) },
+                        CookieAuthenticationDefaults.AuthenticationScheme
+                    ));
+        }
+
+        public static AuthenticationProperties getAdminAuthProperties()
+        {
+            return new AuthenticationProperties
+            {
+                AllowRefresh = true,
+                IssuedUtc = DateTimeOffset.UtcNow,
+                ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(90),
+                IsPersistent = true
+            };
+
+        }
+
+        #endregion
+
+    }
+}
